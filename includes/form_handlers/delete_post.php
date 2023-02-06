5473cbf8b2ddb5b355b9ee3f1efd2c0fbfa62600
apykwan <apykwan@hotmail.com>
2023-02-06 01:22:41 -0800

    allowign user to post image along with text

--- includes/form_handlers/delete_post.php
+++ includes/form_handlers/delete_post.php
@@ -5,6 +5,14 @@ if(isset($_GET['post_id']))
     $post_id = $_GET['post_id'];
  
 if(isset($_POST['result'])) {
-    if($_POST['result'] == 'true')
+    if($_POST['result'] == 'true') {
         $query = mysqli_query($con, "UPDATE posts SET deleted='yes' WHERE id='$post_id'");
+
+        //Deleting image if exist
+        $queryImagePath = mysqli_query($con, "SELECT image FROM posts WHERE id='$post_id'");
+        $row = mysqli_fetch_array($queryImagePath);
+
+        if (is_file($row['image'])) 
+            unlink($row['image']);
+    }
 }
